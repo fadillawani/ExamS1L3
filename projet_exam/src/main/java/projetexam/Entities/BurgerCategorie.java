7a9a3dc9ca170c79fe74f7f8ac49fd25aeaf6a25
fadillawani <152498300+fadillawani@users.noreply.github.com>
2025-12-08 20:00:54 +0000

    Modification de l'entité BurgerCategorie

--- projet_exam/src/main/java/projetexam/Entities/BurgerCategorie.java
+++ projet_exam/src/main/java/projetexam/Entities/BurgerCategorie.java
@@ -1,5 +1,30 @@
 package projetexam.Entities;
 
 public class BurgerCategorie {
-    
+    private int id;
+    private String nom;
+
+    public BurgerCategorie(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public BurgerCategorie() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
